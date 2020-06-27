46010bc1fb80aedca1dcac1ea8a3014f54f57563
Steven Orvis <steven@trappingpa.com>
2020-06-27 09:56:04 -0400

    Fix push in script

--- create_audio_PR.sh
+++ create_audio_PR.sh
@@ -28,6 +28,6 @@ echo 'thumbnail: "/assets/J93hxt0rFsAmq.jpg"' >> $FILE_PATH
 echo 'audio: "https://archive.org/download/sm-'$POST_MONTH'-'$POST_DAY'-'$POST_YEAR'/SM%20'$POST_MONTH'-'$POST_DAY'-'$POST_YEAR'.mp3"' >> $FILE_PATH
 echo '---' >> $FILE_PATH
 
-git push && \
+git push --set-upstream origin $BRANCH_NAME && \
 git checkout master && \
 git branch -D $BRANCH_NAME
